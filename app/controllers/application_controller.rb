151f473f24ae96a72c4804617433b8ebd6368012
John W. Fail <john@pixelache.ac>
2018-01-11 15:13:37 +0200

    sessions tore

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,3 +1,4 @@
 class ApplicationController < ActionController::Base
   protect_from_forgery with: :exception, prepend: true
+  skip_before_action :verify_authenticity_token, if: -> { controller_name == 'sessions' && action_name == 'destroy' }
 end
