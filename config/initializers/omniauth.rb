9ccd9d4a6de3adbdf2e4f72911de1113c4624951
John W. Fail <john@pixelache.ac>
2018-01-11 18:27:24 +0200

    maybe we have a domain name problem

--- config/initializers/omniauth.rb
+++ config/initializers/omniauth.rb
@@ -3,11 +3,11 @@
 
 if Rails.env.production?
   Rails.application.config.middleware.use OmniAuth::Builder do
-    provider :facebook, ENV['facebook_app_id'], ENV['facebook_secret'], callback_url: 'https://bookings.worldmusic.school/users/auth/facebook/callback'
-    provider :twitter, ENV['twitter_consumer_key'], ENV['twitter_consumer_secret'], callback_url: 'https://bookings.worldmusic.school/users/auth/twitter/callback'
+    provider :facebook, ENV['facebook_app_id'], ENV['facebook_secret'], callback_url: 'https://booking.worldmusic.school/users/auth/facebook/callback'
+    provider :twitter, ENV['twitter_consumer_key'], ENV['twitter_consumer_secret'], callback_url: 'https://booking.worldmusic.school/users/auth/twitter/callback'
 
   end
-  OmniAuth.config.full_host = 'https://bookings.worldmusic.school'
+  OmniAuth.config.full_host = 'https://booking.worldmusic.school'
 else
   Rails.application.config.middleware.use OmniAuth::Builder do
     provider :facebook, ENV['facebook_app_id'], ENV['facebook_secret'], callback_url: 'http://wms.school:3000/users/auth/facebook/callback'
