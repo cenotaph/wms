b6da2646124c83c24a46efdc6d29cdfa16bb6cd7
John W. Fail <john@pixelache.ac>
2018-03-13 16:31:33 +0200

    trying to fix sign in problem

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,5 +1,5 @@
 class ApplicationController < ActionController::Base
-  protect_from_forgery with: :exception, prepend: true
+  protect_from_forgery with: :exception #, prepend: true
   skip_before_action :verify_authenticity_token, if: -> { controller_name == 'sessions' && action_name == 'destroy' }
 
   before_action :configure_permitted_parameters, if: :devise_controller?
